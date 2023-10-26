06a652563e969e67e4552592957f2d1c432e0e89
Zia Khan <ziaukhan@hotmail.com>
2023-10-26 15:34:55 +0500

    update step01

--- step01_llm_app_dev/open_ai.py
+++ step01_llm_app_dev/open_ai.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv()) # read local .env file
 openai.api_key: str = os.environ['OPENAI_API_KEY']
 
-llm_model: str = "gpt-3.5-turbo"
+llm_model: str = "gpt-3.5-turbo-0613"
 
 def get_completion(prompt: str, model: str=llm_model)->str:
     messages: [dict[str: str]] = [{"role": "user", "content": prompt}]
 
     # https://cookbook.openai.com/examples/how_to_format_inputs_to_chatgpt_models#2-an-example-chat-api-call
-    response: openai.openai_object.OpenAIObject = openai.ChatCompletion.create(
+    response = openai.ChatCompletion.create(
         model=model,
         messages=messages,
         temperature=0, 
