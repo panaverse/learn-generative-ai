dcf761c9c343435fa902a20a14c5d36ca1484b1c
Sir Qasim <m.qasim077@gmail.com>
2023-11-14 00:34:03 +0500

    update code

--- 02_streaming_response/app.py
+++ 02_streaming_response/app.py
@@ -1,30 +1,11 @@
-from openai import OpenAI
-
-from dotenv import load_dotenv, find_dotenv
-_ : bool = load_dotenv() # read local .env file
-
-
 import asyncio
-from openai import AsyncOpenAI
+async def async_func():
+    print('Velotio ...')
+    await asyncio.sleep(5)
+    print('... Technologies!')
 
-client = AsyncOpenAI(
-    # defaults to os.environ.get("OPENAI_API_KEY")
-    api_key="My API Key",
-)
+async def main():
+    async_func()#this will do nothing because coroutine object is created but not awaited
+    await async_func()
 
-
-async def main() -> None:
-    chat_completion = await client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": "Say this is a test",
-            }
-        ],
-        model="gpt-3.5-turbo",
-    )
-async for part in client:
-    print(part.choices[0].delta.content or "")
-
-
-
+asyncio.run(main())
