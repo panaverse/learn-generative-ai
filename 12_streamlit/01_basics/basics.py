65cb225932a07ad92eee3b5f3c4cbaeb28070913
Zia Khan <ziaukhan@hotmail.com>
2023-11-21 13:58:31 +0500

    completed streamlit 01

--- 12_streamlit/01_basics/basics.py
+++ 12_streamlit/01_basics/basics.py
@@ -61,7 +61,7 @@ if st.checkbox("Show/Hide"):
 # second argument is the options for the radio button
 status = st.radio("Select Gender: ", ('Male', 'Female'))
  
- 
+
 # conditional statement to print 
 # Male if male is selected else print female
 # show the result using the success function
@@ -69,5 +69,72 @@ if (status == 'Male'):
     st.success("Male")
 else:
     st.success("Female")
+
+
+
+# Selection box
+ 
+# first argument takes the title of the selectionbox
+# second argument takes options
+hobby = st.selectbox("Hobbies: ",
+                     ['Dancing', 'Reading', 'Sports'])
+ 
+# print the selected hobby
+st.write("Your hobby is: ", hobby)
+
+
+
+# multi select box
+ 
+# first argument takes the box title
+# second argument takes the options to show
+hobbies = st.multiselect("Hobbies: ",
+                         ['Dancing', 'Reading', 'Sports'])
+ 
+# write the selected options
+st.write("You selected", len(hobbies), 'hobbies')
+
+
+
+
+# Create a simple button that does nothing
+st.button("Click me for no reason")
+ 
+
+# Create a button, that when clicked, shows a text
+if(st.button("About")):
+    st.text("Welcome To World of Generative AI!!!")
+
+
+
+# Text Input
+ 
+# save the input text in the variable 'name'
+# first argument shows the title of the text input box
+# second argument displays a default text inside the text input area
+name = st.text_input("Enter Your name", "Type Here ...")
+ 
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+    result = name.title()
+    st.success(result)
+
+
+
+
+# slider
+ 
+# first argument takes the title of the slider
+# second argument takes the starting of the slider
+# last argument takes the end number
+level = st.slider("Select the level", 1, 5)
+ 
+# print the level
+# format() is used to print value 
+# of a variable at a specific position
+st.text('Selected: {}'.format(level))
+
+
 
 
