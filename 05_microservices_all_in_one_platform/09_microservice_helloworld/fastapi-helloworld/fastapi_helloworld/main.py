5b941e54d5f7d8ad35805fcb56977e6817727736
Zia Khan <ziaukhan@hotmail.com>
2024-02-27 09:11:57 +0500

    added gpt direction in hello world api

--- 05_microservices_all_in_one_platform/09_microservice_helloworld/fastapi-helloworld/fastapi_helloworld/main.py
+++ 05_microservices_all_in_one_platform/09_microservice_helloworld/fastapi-helloworld/fastapi_helloworld/main.py
@@ -4,15 +4,17 @@ from typing import Union
 
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="Hello World API", 
+    version="0.0.1",
+    servers=[
+        {
+            "url": "http://0.0.0.0:8000", # ADD NGROK URL Here Before Creating GPT Action
+            "description": "Development Server"
+        }
+        ])
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-
-    return {"item_id": item_id, "q": q}
