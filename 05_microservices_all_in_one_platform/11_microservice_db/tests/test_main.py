48aafb2b9931bf82a09a95b5acec850abf24f973
Zia Khan <ziaukhan@hotmail.com>
2024-02-29 18:17:27 +0500

    update

--- 05_microservices_all_in_one_platform/11_microservice_db/tests/test_main.py
+++ 05_microservices_all_in_one_platform/11_microservice_db/tests/test_main.py
@@ -1,23 +1,35 @@
 from fastapi.testclient import TestClient
 
-from fastapi_neon.main import app
-
-client = TestClient(app=app)
+# https://sqlmodel.tiangolo.com/tutorial/fastapi/tests/#override-a-dependency
+from fastapi_neon.main import app, get_session
 
 # https://fastapi.tiangolo.com/tutorial/testing/
+# https://realpython.com/python-assert-statement/
+# https://understandingdata.com/posts/list-of-python-assert-statements-for-unit-tests/
 
 def test_read_main():
+    client = TestClient(app=app)
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
 def test_write_main():
+    client = TestClient(app=app)
+
+    todo_content = "buy bread"
+
     response = client.post("/todos/",
-        json={"content": "buy apples"}
+        json={"content": todo_content}
     )
+
+    data = response.json()
+
     assert response.status_code == 200
+    assert data["content"] == todo_content
 
 def test_read_list_main():
+    client = TestClient(app=app)
+
     response = client.get("/todos/")
     assert response.status_code == 200
     
